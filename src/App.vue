e676741dc0b8b22ce00ae95061adeb3604eb1a78
YongBF <1727132050@qq.com>
2018-12-11 18:09:35 +0800

    项目初始化及搜索框布局

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import url('//at.alicdn.com/t/font_960852_gft8etvzmid.css');
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.small-icon {
+  font-size: 0.8rem;
+}
+
+.normal-icon {
+  font-size: 1rem;
+}
+
+.large-icon {
+  font-size: 1.3rem;
+}
+</style>
---
+++ src/components/ShoppingMall.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="search-bar">
+		<van-row gutter="5">
+			<van-col span="3"><i class="iconfont icon-locationfill noraml-icon"></i></van-col>
+			<van-col span="16">
+				<input type="text" class="search-input"/>
+			</van-col>
+			<van-col span="5">搜索</van-col>
+		</van-row>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			msg: "welcome to e-business"
+		};
+	}
+};
+</script>
+
+<style scoped>
+.search-bar{
+    height: 2.2rem;
+    background-color: #109ee8;
+	line-height:2.2rem;
+	font-size: 0.9rem;
+	color: #fff;
+}
+
+.search-input{
+      width:100%;
+	  height: 1.4rem;
+	  border-radius: 1.4rem;
+	  background: #fff;
+	  border: 0;
+	  padding: 0 0.7rem;
+	  box-sizing: border-box;
+	  font-size: 0.9rem;
+	  color: #666;
+  }
+</style>
